b57db090540c7f1493feba8d62b5908721764695
jyotish.mohanlal4@gmail.com <jyotish.mohanlal4@gmail.com>
2026-09-18 16:21:55 +0500

    Added test cases

--- is_array_sorted.cpp
+++ is_array_sorted.cpp
@@ -3,4 +3,20 @@
 
 bool isSorted(const int* arr, const int size);
 
-int main() {} 
+int main() {
+
+
+
+	assert(isSorted({1, 2, 3}, 3) == true);
+	assert(isSorted({1, 3, 2}, 3) == false);
+	assert(isSorted({1, 2, 2}, 3) == true);
+	assert(isSorted({1}, 1) == true);
+	assert(isSorted({3, 2 ,1}, 3) == false);
+	assert(isSorted({-3, -2, -1}, 3) == true);
+
+	// Containing no elements
+	assert(isSorted({}, 0) == true);
+
+	// Negative size
+	assert(isSorted({1, 3}, -1) == false);
+} 
